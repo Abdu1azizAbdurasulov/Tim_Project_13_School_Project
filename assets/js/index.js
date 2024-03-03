fc101d5cd18a45e2ebfa3ad4b8626f69f924e358
Abdu1azizAbdurasulov <abdurasulovabdulazizdev@gmail.com>
2024-03-03 14:56:04 +0500

    I'm complete hamburger menu animation

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,16 @@
 const hamburgerBtn = document.querySelector(".hamburger__wrap"),
- 	hamburgerMenu = document.querySelector(".nav_wrapper"),
+ 	hamburgerMenu = document.querySelector(".nav_menu"),
 	closeBtn = document.querySelector(".x_mark"),
 	show = document.querySelector(".show"),
 	hide = document.querySelector(".hidden");
 
 hamburgerBtn.addEventListener("click", () =>{
-	
+	hamburgerMenu.style.display = "block"
+	hamburgerBtn.style.display = "none"
+	closeBtn.style.display = "block"
 });
 closeBtn.addEventListener("click", () =>{
-	
+	hamburgerMenu.style.display = "none"
+	hamburgerBtn.style.display = "block"
+	closeBtn.style.display = "none"
 });
-
